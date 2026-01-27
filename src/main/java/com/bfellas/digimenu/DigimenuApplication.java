3d76af1aca0068b771376d5d319f08ed7338367a
#noormubash786 <adnanshariqmd@gmail.com>
2026-01-27 11:13:47 +0530

    scan all packages

--- src/main/java/com/bfellas/digimenu/DigimenuApplication.java
+++ src/main/java/com/bfellas/digimenu/DigimenuApplication.java
@@ -3,11 +3,17 @@ package com.bfellas.digimenu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "com.bfellas.digimenu",
+        "controller",
+        "service",
+        "repository",
+        "entity",
+        "dto"
+})
 public class DigimenuApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DigimenuApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DigimenuApplication.class, args);
+    }
 }
