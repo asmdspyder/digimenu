5a54c0e6099811fd5442a02cade8200db594e603
#noormubash786 <adnanshariqmd@gmail.com>
2026-01-27 11:41:58 +0530

    package scan outside

--- src/main/java/com/bfellas/digimenu/DigimenuApplication.java
+++ src/main/java/com/bfellas/digimenu/DigimenuApplication.java
@@ -3,11 +3,19 @@ package com.bfellas.digimenu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "repository")
 @EntityScan(basePackages = "entity")
+@ComponentScan(basePackages = {
+        "controller",
+        "service",
+        "repository",
+        "entity",
+        "dto"
+})
 public class DigimenuApplication {
 
     public static void main(String[] args) {
